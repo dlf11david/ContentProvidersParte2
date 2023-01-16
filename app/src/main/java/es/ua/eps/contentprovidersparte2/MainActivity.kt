0ec4848e345018039802df7dffbc3015d1c2defe
davidlopez <dfl11@alu.ua.es>
2023-01-16 23:07:48 +0100

    Primer intento

--- app/src/main/java/es/ua/eps/contentprovidersparte2/MainActivity.kt
+++ app/src/main/java/es/ua/eps/contentprovidersparte2/MainActivity.kt
@@ -1,11 +1,45 @@
 package es.ua.eps.contentprovidersparte2
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import es.ua.eps.contentprovidersparte2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        val PROVIDER_NAME = "es.ua.eps.sqlite/ProviderCode"
+        val URL = "content://$PROVIDER_NAME/usuarios"
+        val CONTENT_URI = Uri.parse(URL)
+        val _ID = "_id"
+        val NOMBRE_USUARIO = "nombre_usuario"
+        val PASSWORD = "password"
+        val NOMBRE_COMPLETO = "nombre_completo"
+        val EMAIL = "email"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val cursor = contentResolver.query(
+            CONTENT_URI,
+            arrayOf(NOMBRE_USUARIO, EMAIL),
+            null,
+            null,
+            null)
+        with(binding) {
+            texto.text = "Name & Email: \n"
+
+            if (cursor!!.moveToFirst()) {
+                do {
+                    texto.append(cursor.getString(0).toString()+": ")
+                    texto.append(cursor.getString(1).toString()+"\n")
+                } while (cursor.moveToNext())
+            }
+
+            cursor.close()
+
+
+
+        }
     }
 }
